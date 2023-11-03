6b44f9b7bf8fcfd337de1f269778f1ad50822a1d
鼓励师 <mr_zhang522@163.com>
2023-11-03 16:39:11 +0800

    fix: 暂时注释@Log，允许他们战斗

--- src/main/java/net/lesscoding/controller/BattleController.java
+++ src/main/java/net/lesscoding/controller/BattleController.java
@@ -1,18 +1,19 @@
 package net.lesscoding.controller;
 
-import net.lesscoding.aspect.Log;
 import net.lesscoding.common.Result;
 import net.lesscoding.common.ResultFactory;
-import net.lesscoding.enums.BusinessType;
 import net.lesscoding.model.dto.BattleDto;
 import net.lesscoding.service.BattleService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 /**
+ * 战斗接口
+ *
  * @author eleven
  * @date 2023/9/27 12:15
  * @apiNote
@@ -20,11 +21,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/battle")
 public class BattleController {
-    @Autowired
+    @Resource
     private BattleService battleService;
 
+    /**
+     * 发起战斗
+     *
+     * @author 鼓励师
+     * @date 2023/11/1 13:53
+     */
     @PostMapping("/doBattle")
-    @Log(title = "PVP战斗", businessType = BusinessType.INSERT)
+//    @Log(title = "PVP战斗", businessType = BusinessType.INSERT)
     private Result doBattle(@RequestBody BattleDto dto) {
         return ResultFactory.success(battleService.doBattle(dto));
     }
